1db5beae00c0183d0bd28003cdffdd58bfcb28a6
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2025-10-17 07:32:17 +0000

    Deploying to gh-pages from @ LOSKIw/LOSKIw.github.io@f46d20e2f93e9bd42ed5fb3e4d1dde2f1d562a82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "Archive.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "dropdown-life",
               title: "life",
               description: "",
